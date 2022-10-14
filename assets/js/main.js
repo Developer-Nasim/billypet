2cda9d5e1c8bdac7c0f99b8610a032326028366d
Developer-Nasim <ajnasim72@gmail.coom>
2022-10-15 00:33:27 +0600

    all pages done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 (function($) {
   "use strict";
   
+  let btns = document.querySelectorAll('.choseSteps_bottom a')
+  if (btns.length > 0) { 
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            let allFrm = document.querySelectorAll('.form')
+            allFrm.forEach(frm => {
+                frm.classList.remove('show')
+            })
+            let keepIt = e.target.href 
+            let keepItTrimed = keepIt.slice(keepIt.indexOf('#')+1)
+            let findDiv = document.querySelector("."+keepItTrimed)
+            console.log(findDiv)
+            if (findDiv) {
+                findDiv.classList.add('show')
+            }
+
+        })
+    })
+  }
   $('select').niceSelect();
 
  
